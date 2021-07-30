d53f25d007d371420b107603a3a0cef96ef92290
Kevin Garcia <kevinede40@gmail.com>
2021-07-30 12:43:30 -0600

    Adding and showing saved tasks

--- js/index.js
+++ js/index.js
@@ -1,18 +1,53 @@
+//crear elementos en la lista
+let crearElementosLista = ((elemento) => {
+    let contenido = document.createTextNode(elemento)
+    let tasksDoing = document.getElementById("tasks-doing")
+    let taskItem = document.createElement("article")
+    taskItem.className += "tasks-item"
+    taskItem.appendChild(contenido)
+    tasksDoing.appendChild(taskItem)
+    console.log(elemento)
+
+})
+
 if(localStorage.getItem('prueba') != null){
     console.log('prueba existe')
+
+    let prueba = JSON.parse(localStorage.getItem("prueba"))
+
+    prueba.forEach(item => {
+        crearElementosLista(item);
+    });
+    
 }else{
     let prueba = []
     localStorage.setItem("prueba", JSON.stringify(prueba))
     console.log('else se ejecutó')
 }
-let add = ((text) => {
-  var agregar = text;
-  let prueba = JSON.parse(localStorage.getItem("prueba"))
-  prueba.push(agregar)
-  console.log(prueba)
-  localStorage.setItem("prueba", JSON.stringify(prueba))
+
+
+
+//Función para añadir elementos al array
+let add = (() => {
+    let input = document.getElementById("task-input").value
+    if(input === ""){
+        alert('Ingrese una tarea')
+    }else{
+        let prueba = JSON.parse(localStorage.getItem("prueba"))
+        prueba.push(input)
+        localStorage.setItem("prueba", JSON.stringify(prueba))
+        document.getElementById("task-input").value = "";
+
+        let contenido = document.createTextNode(input)
+        let tasksDoing = document.getElementById("tasks-doing")
+        let taskItem = document.createElement("article")
+        taskItem.className += "tasks-item"
+        taskItem.appendChild(contenido)
+        tasksDoing.appendChild(taskItem)
+    }
 });
 
+//Borrar elementos del array
 let borrar = ((tarea) => {
     let prueba = JSON.parse(localStorage.getItem("prueba"))
     prueba.splice(tarea, 1)
