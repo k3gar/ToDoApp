62ba8a2106bcc6d9650a373d3ced648de53d2f80
Kevin Garcia <kevinede40@gmail.com>
2021-07-30 13:46:07 -0600

    Adding X

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-//crear elementos en la lista
+//Crear elementos en la lista
 let crearElementosLista = ((elemento) => {
     let contenido = document.createTextNode(elemento)
     let tasksDoing = document.getElementById("tasks-doing")
@@ -6,26 +6,29 @@ let crearElementosLista = ((elemento) => {
     taskItem.className += "tasks-item"
     taskItem.appendChild(contenido)
     tasksDoing.appendChild(taskItem)
+    let img = document.createElement("img");
+    img.src = "../img/close.svg"
+    img.className += "img"
+    taskItem.appendChild(img)
     console.log(elemento)
-
 })
 
+
+//Revisa si el array vacío existe. 
+//Si existe, nos imprime los elementos del array que ya existen.
+//Si no existe, lo crea
 if(localStorage.getItem('prueba') != null){
     console.log('prueba existe')
-
     let prueba = JSON.parse(localStorage.getItem("prueba"))
 
     prueba.forEach(item => {
         crearElementosLista(item);
     });
-    
 }else{
     let prueba = []
     localStorage.setItem("prueba", JSON.stringify(prueba))
     console.log('else se ejecutó')
 }
-
-
 
 //Función para añadir elementos al array
 let add = (() => {
@@ -37,13 +40,7 @@ let add = (() => {
         prueba.push(input)
         localStorage.setItem("prueba", JSON.stringify(prueba))
         document.getElementById("task-input").value = "";
-
-        let contenido = document.createTextNode(input)
-        let tasksDoing = document.getElementById("tasks-doing")
-        let taskItem = document.createElement("article")
-        taskItem.className += "tasks-item"
-        taskItem.appendChild(contenido)
-        tasksDoing.appendChild(taskItem)
+        crearElementosLista(input)
     }
 });
 
